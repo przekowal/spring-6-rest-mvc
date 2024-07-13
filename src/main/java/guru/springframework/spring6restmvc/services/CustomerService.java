8d732a978cfac6c0792c55cda43dcf13d7480f16
Marcin <kowalski.marcin.r@gmail.com>
2024-07-13 13:41:42 +0200

    add Customer functionality

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerService {
+    Optional<Customer> getCustomerById(UUID uuid);
+
+    List<Customer> getAllCustomers();
+
+    Customer saveNewCustomer(Customer customer);
+
+    void updateCustomerById(UUID customerId, Customer customer);
+
+    void deleteCustomerById(UUID customerId);
+
+    void patchCustomerById(UUID customerId, Customer customer);
+}
